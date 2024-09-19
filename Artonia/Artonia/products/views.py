c9b4a12e79a334ae71d8389e15e3cacab0fbd704
YordanDobrev <petolisko@gmail.com>
2024-09-19 10:35:08 +0300

    more solutions added

--- Artonia/Artonia/products/views.py
+++ Artonia/Artonia/products/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Artonia.products.models import ArtPainting, Macrame
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    art_paintings = request.GET.get('art_paintings', '')
+    art_paintings = ArtPainting.objects.filter(name__icontains=art_paintings)
+
+    macrame = request.GET.get('macrame', '')
+    macrame = ArtPainting.objects.filter(name__icontains=macrame)
+
+    context = {
+        'art_paintings': art_paintings,
+        'macrame': macrame,
+    }
+
+    return render(request, 'products.html', context)
