e3bf4fc5a10dfe1d92a25cbfad12d5ea12ae433c
YordanDobrev <petolisko@gmail.com>
2024-10-27 12:37:39 +0200

    Post edit and deletion logic implemented

--- RegularExam/RegularExam/common/views.py
+++ RegularExam/RegularExam/common/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import BaseFormView
 
 from RegularExam.author.forms import AuthorCreateForm
 from RegularExam.post.models import Post
+from RegularExam.utils import get_user_obj
 
 
 # Create your views here.
@@ -12,9 +14,36 @@ class HomePage(ListView, BaseFormView):
     form_class = AuthorCreateForm
     success_url = reverse_lazy('home')
 
-    def get_template_names(self):
-        return ['index.html']
+    def get_template_names(self, **kwargs):
+        profile = get_user_obj()
+        if not profile:
+            return ['index.html']
+
+        return ['dashboard.html']
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+# def dashboard(request):
+#     profile = get_user_obj()
+#     posts = Post.objects.filter(pk=profile.pk)
+#
+#     context = {
+#         'profile': profile,
+#         'posts': posts,
+#     }
+#
+#     render(request, 'dashboard.html', context)
+
+def dashboard(request):
+    profile = get_user_obj()
+    posts = Post.objects.filter(pk=profile.pk)
+
+    context = {
+        'profile': profile,
+        'posts': posts,
+    }
+
+    return render(request, 'dashboard.html', context)
--- RegularExam/RegularExam/post/urls.py
+++ RegularExam/RegularExam/post/urls.py
@@ -4,10 +4,10 @@ from RegularExam.post import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('create/', views.create_post, name='create_post'),
+    path('create/', views.PostCreateView.as_view(), name='create_post'),
     path('<int:id>/', include([  #TO ASK IN SLIDO IF THIS IS A SLUG OR THE ID OF THE POST
-        path('details/', views.details_post, name='details_post'),
-        path('edit/', views.edit_post, name='edit_post'),
-        path('delete/', views.delete_post, name='delete_post'),
+        path('details/', views.PostDetailsView.as_view(), name='details_post'),
+        path('edit/', views.PostEditView.as_view(), name='edit_post'),
+        path('delete/', views.PostDeleteView.as_view(), name='delete_post'),
     ])),
 ]
