748095d8bd6957d72433e66500ccf88afc0d8f8e
Yordan Dobrev <145679398+YordanDobrev@users.noreply.github.com>
2024-12-11 18:47:03 +0200

    Delete music_app_exam_prep directory

--- music_app_exam_prep/music_app_exam_prep/common/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from music_app_exam_prep.common import views
-
-urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
-]
--- music_app_exam_prep/music_app_exam_prep/utils.py
+++
@@ -1,5 +0,0 @@
-from music_app_exam_prep.profiles.models import Profile
-
-
-def get_user_obj():
-    return Profile.objects.first()
